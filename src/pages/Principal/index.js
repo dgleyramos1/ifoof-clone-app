b06b6a306bd1fb6f11d31e0488101cb47f15c416
Dgley Ramos <dgleysilva@gmail.com>
2022-02-24 21:55:18 -0300

    Consumindo api fake com useEffect e useState

--- src/pages/Principal/index.js
+++ src/pages/Principal/index.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Text, Alert, ActivityIndicater } from 'react-native';
+
 import { SafeAreaView, Image, View, ButtonContainer } from './style.js';
 
 import { Text } from 'react-native';
@@ -8,6 +10,45 @@ import Button from '../../components/Button';
 
 
 export default function Principal() {
+
+  const [banners, setBanners] = useState([])
+  const [categorias, setCategorias] = useState([])
+  const [restaurantes, setRestaurantes] = useState([])
+  const [loaded, setLoaded] = useState(false)
+  const [tipo, setTipo] = useState('Entrega');
+
+  useEffect(() => {
+
+
+    async function buscaDados() {
+
+      try {
+
+        const response = await fetch('http://my-json-server.typicode.com/pablohdev/app-ifood-clone/db');
+
+        const data = await response.json();
+
+
+        setLoaded(true)
+
+
+        setBanners(data.banner_principal);
+        setCategorias(data.categorias);
+        setRestaurantes(data.restaurantes);
+
+
+      } catch (e) {
+
+        Alert.alert('Erro consultada' + e);
+
+      }
+
+    }
+
+    buscaDados()
+
+
+  }, [])
   return (
     <>
       <StatusBar style='theme-dark' />
